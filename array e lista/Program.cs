cce24463f45d39a18a79159a5229c3441bc30fe3
Giovanna Simione <giovannasimionero@gmail.com>
2023-07-27 11:40:33 -0400

    explicação array e list

---
+++ array e lista/Program.cs
@@ -0,0 +1,29 @@
+﻿//*double[] n = new double [3]; //array
+
+//Console.WriteLine("Digite sua nota: ");
+//n[0] = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Digite sua nota: ");
+//n[1]= Convert.ToDouble(Console.ReadLine());
+
+//n[2] = (n[0] + n[1]) / 2;
+
+
+//Console.WriteLine(n[2]); 
+
+List<string> list = new List<string>();
+list.Add("pedro");
+list.Add("cassiane");
+list.Add("enzo");
+
+Console.WriteLine(list[0]);
+Console.WriteLine(list.Count);
+
+
+foreach (string str in list)
+{
+    Console.WriteLine(str);
+}
+
+Console.ReadKey();
+
